f3132d5f155d2d1e1c7b8e62065495242267fb32
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-09-22 22:40:50 +0530

    navbar & footer

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    const createLogo = (name: string) =>
+    name
+        .split(" ")
+        .map((word) => word[0])
+        .join("")
+        .toUpperCase();
+</script>
+
+<header class="container flex items-center justify-between h-[80px] sm:h-[123px]">
+    <a class="text-2xl dark:text-light font-semibold dark:font-semibold font-outfit" href="/">
+        {createLogo("H B")}.
+    </a>
+    <nav>
+        <ul class="flex items-center">
+            <li>
+                <a class="p-4 dark:text-light block text-lg" href="/projects">
+                    Projects
+                </a>
+            </li>
+            <li>
+                <a class="p-4 dark:text-light block text-lg" href="/blog">
+                    Blogs
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.css";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
-<slot />
+<main class="container max-w-6xl px-4 min-h-screen">
+    <Navbar />
+    <slot />
+    <Footer />
+</main>
