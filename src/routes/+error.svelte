e066bf12a68af5d124cd4614e72c6ddc15d77fad
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-09-29 21:14:55 +0530

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="min-h-[calc(100vh-210px)] sm:min-h-[calc(100vh-270px)]">
+    <h1>{$page?.error?.message}</h1>
+</section>
+
