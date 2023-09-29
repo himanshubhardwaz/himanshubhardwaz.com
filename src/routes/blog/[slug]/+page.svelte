0f4528a6e2e5d65a727a83ea40b5debc2b2c5b23
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-09-29 20:41:54 +0530

    Shawdow dom for blog section

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,15 +1,31 @@
-<script lang="ts">
+<script anyscript lang="ts">
 	import type { PageData } from "./$types";
-    import {marked} from "marked";
+    import { marked } from "marked";
+    import { onMount } from 'svelte';
 
-    export let data: PageData
+    export let data: PageData;
+
+    onMount(() => {
+        const blogSection = document.getElementById("blog-section");
+
+        let blogShadowRoot: ShadowRoot | null  = null;
+
+        if(blogSection){
+            blogShadowRoot = blogSection.attachShadow({mode: "open"});
+        }
+
+        if(blogShadowRoot){
+            const blog = document.createElement("div");
+            blog.innerHTML = marked(data.content);
+            blog.style.textAlign = "justify";
+            blogShadowRoot.appendChild(blog);
+        }
+    });
 </script>
 
 <svelte:head>
 	<title>{data.title} by Himanshu</title>
-     <meta name="description" content={`Blog: ${data.title} by Himanshu`}>
+    <meta name="description" content={`Blog: ${data.title} by Himanshu`}>
 </svelte:head>
 
-<section>
-   {@html marked(data.content)}
-</section>
+<section id="blog-section" />
