0c1688ed5899996e93fe4cadadc1ca199edc0026
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-12-28 01:06:29 +0530

    error handled

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,23 +2,32 @@ import { getDB, closeDB } from '$lib/server/db';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const db = await getDB();
+	try {
+		const db = await getDB();
 
-	const route = event.url.href;
+		const route = event.url.href;
 
-	await db.run(
-		`
+		await db.run(
+			`
 		INSERT INTO page_views (route, count)
 		VALUES (?, 1)
 		ON CONFLICT(route)
 		DO UPDATE SET count = COALESCE(count, 0) + 1;
 		`,
-		[route]
-	);
+			[route]
+		);
 
-	const response = await resolve(event);
+		const response = await resolve(event);
 
-	await closeDB();
+		await closeDB();
 
-	return response;
+		return response;
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error(error.message);
+		}
+		const response = await resolve(event);
+
+		return response;
+	}
 };
