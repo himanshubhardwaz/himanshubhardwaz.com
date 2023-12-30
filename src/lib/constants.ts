1857c9d28232f4bd97de07c962ae593f76ac58c9
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-12-30 09:55:04 +0530

    meta tags for resume

---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const RESUME_LINK = "https://himanshubhardwaz.github.io/resume/Himanshu's_Resume.pdf";
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,11 +1,27 @@
 <script>
   import { goto } from '$app/navigation';
   import { onMount } from "svelte"
+  import { RESUME_LINK } from "$lib/constants"
 
   onMount(() => {
-      goto('https://himanshubhardwaz.github.io/resume/Himanshu\'s_Resume.pdf', );
+      goto(RESUME_LINK);
   });
 </script>
+
+<svelte:head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Himanshu Bhardwaz - Software Engineer | React.js, Node.js, TypeScript | SDE at SaaS Labs US Inc.">
+    <meta name="keywords" content="Himanshu Bhardwaz, Software Engineer, React.js, Node.js, TypeScript, Full Stack Developer, Web Developer">
+    <meta name="author" content="Himanshu Bhardwaz">
+    <meta name="robots" content="index, follow">
+    <meta name="og:title" content="Himanshu Bhardwaz - Software Engineer Resume">
+    <meta name="og:description" content="Explore Himanshu Bhardwaz's skills, experience, and achievements in software development.">
+    <meta name="og:image" content="/resume_preview.png">
+    <meta name="og:url" content={RESUME_LINK}>
+    <meta name="og:type" content="website">
+    <title>Himanshu Bhardwaz - Software Engineer Resume</title>
+</svelte:head>
 
 <div class="fixed top-[50%] left-[50%]">
   Redirecting...
