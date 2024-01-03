433c94a7f9d3712bb17ea65c37da14b335b9423f
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-01-03 20:42:36 +0530

    redirecting fixed

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -23,7 +23,7 @@
     <title>Himanshu Bhardwaz - Software Engineer Resume</title>
 </svelte:head>
 
-<div class="fixed top-[50%] left-[50%]">
+<div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
   Redirecting...
 </div>
 
