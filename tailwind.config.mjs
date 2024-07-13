0fd0441b81d82cc4079a3ace2bf77c45f698bd1f
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-07-13 21:20:14 +0530

    base layout, tailwind init

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "media",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "var(--color-bg)",
+        "bg-subtle": "var(--color-bg-subtle)",
+        border: "var(--color-border)",
+        text: "var(--color-text)",
+        "text-subtle": "var(--color-text-subtle)",
+        gray: colors.zinc,
+      },
+      fontFamily: {
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+        mono: ["JetBrains Mono Variable", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
