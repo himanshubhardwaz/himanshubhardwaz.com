16199a5d8d57337e58c4a66bfec1122c8b5da585
Himanshu Bhardwaz <himanshu76200@gmail.com>
2024-12-16 08:41:26 +0530

    SITE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
-  site: "https://himanshubhardwaz.com",
+  site: process.env.SITE_URL,
   adapter: cloudflare(),
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
