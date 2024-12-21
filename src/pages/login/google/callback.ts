c729970b7b571dc5e7a9461d6c357c19a46fe67c
Himanshu Bhardwaz <himanshu76200@gmail.com>
2024-12-21 23:01:57 +0530

    logs

--- src/pages/login/google/callback.ts
+++ src/pages/login/google/callback.ts
@@ -13,14 +13,14 @@ import {
   getUserFromGoogleId,
 } from "~/lib/server/user-session-crud";
 
-export const prerender = false;
-
 export async function GET(context: APIContext): Promise<Response> {
+  console.log("here 1");
   const code = context.url.searchParams.get("code");
   const state = context.url.searchParams.get("state");
   const storedState = context.cookies.get("google_oauth_state")?.value ?? null;
   const codeVerifier =
     context.cookies.get("google_code_verifier")?.value ?? null;
+  console.log("here 2", code, state, storedState, codeVerifier);
   if (
     code === null ||
     state === null ||
@@ -31,12 +31,13 @@ export async function GET(context: APIContext): Promise<Response> {
       status: 400,
     });
   }
+  console.log("here 3");
   if (state !== storedState) {
     return new Response(null, {
       status: 400,
     });
   }
-
+  console.log("here 4");
   let tokens: OAuth2Tokens;
   try {
     tokens = await google.validateAuthorizationCode(code, codeVerifier);
@@ -45,6 +46,7 @@ export async function GET(context: APIContext): Promise<Response> {
       status: 400,
     });
   }
+  console.log("here 5");
   const claims = decodeIdToken(tokens.idToken()) as {
     sub: string;
     name: string;
@@ -52,7 +54,11 @@ export async function GET(context: APIContext): Promise<Response> {
   const googleId = claims.sub;
   const userName = claims.name;
 
+  console.log("here 6: ", { googleId, userName });
+
   const existingUser = await getUserFromGoogleId(googleId);
+
+  console.log("here 7: ", existingUser);
 
   if (existingUser !== null) {
     const sessionToken = generateSessionToken();
@@ -61,10 +67,17 @@ export async function GET(context: APIContext): Promise<Response> {
     return context.redirect("/");
   }
 
+  console.log("here 8: ");
+
   const user = await createUser({ googleId, userName });
+
+  console.log("here 9: ", user);
 
   const sessionToken = generateSessionToken();
   const session = await createSession(sessionToken, user.id);
+
+  console.log("here 10: ", user);
   setSessionTokenCookie(context, sessionToken, session.expiresAt);
+  console.log("here 11: ");
   return context.redirect("/");
 }
